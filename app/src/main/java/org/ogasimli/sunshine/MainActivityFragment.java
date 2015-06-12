6e4e7e2b72306d379689bd058cc6e778a37a7292
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-06-12 13:43:31 +0500

    Bind arrayadapter to listview and correct the ID of listview

--- app/src/main/java/org/ogasimli/sunshine/MainActivityFragment.java
+++ app/src/main/java/org/ogasimli/sunshine/MainActivityFragment.java
@@ -1,11 +1,12 @@
 package org.ogasimli.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,6 +24,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         String[] forecastArray = {
                 "Mon 6/23?- Sunny - 31/17",
                 "Tue 6/24 - Foggy - 21/8",
@@ -42,6 +44,9 @@ public class MainActivityFragment extends Fragment {
                         R.id.list_item_forecast_textview, // The ID of the textview to populate.
                         weekForecast);
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 }
