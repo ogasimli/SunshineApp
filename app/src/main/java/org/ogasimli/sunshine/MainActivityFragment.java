e2c41de16d8ff26a8c371b309a0d0025f44055ab
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-06-12 13:20:49 +0500

    Turn array to arraylist

--- app/src/main/java/org/ogasimli/sunshine/MainActivityFragment.java
+++ app/src/main/java/org/ogasimli/sunshine/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -27,6 +31,9 @@ public class MainActivityFragment extends Fragment {
                 "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
                 "Sun 6/29 - Sunny - 20/7"
         };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
