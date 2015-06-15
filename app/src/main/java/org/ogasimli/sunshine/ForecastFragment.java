77026c251f300c04704d95b6fe859effb1b538e6
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-06-16 00:58:47 +0500

    Add setOnItemClickListener to listview

--- app/src/main/java/org/ogasimli/sunshine/ForecastFragment.java
+++ app/src/main/java/org/ogasimli/sunshine/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -92,8 +94,15 @@ public class ForecastFragment extends Fragment {
                         weekForecast);
 
         //Find listview and pass ArrayAdapter to it
-        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        final ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getActivity(),forecastAdapter.getItem(position),Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
 
         return rootView;
     }
