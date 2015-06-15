7b5d5777af4015ad789b4fb89331772c3a3bcada
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-06-15 18:02:06 +0500

    Inflate refresh menu and link FetchWetherTask to it

--- app/src/main/java/org/ogasimli/sunshine/ForecastFragment.java
+++ app/src/main/java/org/ogasimli/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,29 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            FetchWetherTask fetchWetherTask = new FetchWetherTask();
+            fetchWetherTask.execute();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
